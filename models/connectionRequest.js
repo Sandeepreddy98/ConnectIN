bcb07667be79f8bcedcdb396ad1529f38bed807e
Sandeep <sandeep.g@melento.com>
2024-10-18 14:31:41 +0530

    [MNT] Fixed the bugs in the the existing connection request API.

--- models/connectionRequest.js
+++ models/connectionRequest.js
@@ -15,12 +15,13 @@ const connectionRequestSchema = new mongoose.Schema({
 },{timestamps : true}
 )
 
-connectionRequestSchema.pre('save', function (next){
-    const data = this
-    if(data.fromUserId === data.toUserId){
-        throw new Error({message : "Connection request cannot be sent to yourself"});
+connectionRequestSchema.pre("save", function (next) {
+    const connectRequest = this;
+    if (connectRequest.fromUserId.equals(connectRequest.toUserId)) {
+        throw new Error("Connection request cannot be sent to yourself");
     }
-})
+    next();
+});
 
 connectionRequestSchema.index({fromUserId : 1,toUserId : 1})
 const ConnectionRequestModel = mongoose.model('connectionrequest',connectionRequestSchema)
--- routes/request.js
+++ routes/request.js
@@ -2,28 +2,29 @@ const express = require('express');
 const { userAuth } = require('../middlewares/auth');
 const User = require('../models/user');
 const requestRouter = express.Router();
-const ConnectionRequest = require('../models/connectionRequest')
+const ConnectionRequest = require('../models/connectionRequest');
+const ConnectionRequestModel = require('../models/connectionRequest');
 
 requestRouter.post('/send/:status/:toUserId', userAuth, async (req, res) => {
     try {
         const allowedRequestTypes = ["ignored", "interested"]
-        if (!allowedRequestTypes.includes(req.status)) {
-            throw new Error({message : 'Connection request type not allowed'});
+        if (!allowedRequestTypes.includes(req.params.status)) {
+            throw new Error('Connection request type not allowed');
             
         }
         const toUserData = await User.findById(req.params.toUserId);
         if(!toUserData){
-            throw new Error({message : "Connect requested user doesn't exist"});
+            throw new Error("Connect requested user doesn't exist");
         }
-        const existingConnectionrequest = await User.find(
+        const existingConnectionrequest = await ConnectionRequestModel.findOne(
             {$or : [
                 {fromUserId : req.user._id,toUserId : req.params.toUserId},
                 {fromUserId : req.params.toUserId,toUserId : req.user._id}
             ]}
         )
+        
         if(existingConnectionrequest){
-            throw new Error({message : "Connection request already exist"});
-            
+            throw new Error("Connection request already exist");
         }
         const connectionRequest = await new ConnectionRequest({
             fromUserId : req.user._id,
@@ -31,7 +32,7 @@ requestRouter.post('/send/:status/:toUserId', userAuth, async (req, res) => {
             status : req.params.status
         })
         const connectionRequestData = await connectionRequest.save()
-        res.send({
+        res.json({
           message:
             req.params.status === "interested"
               ? `${req.user.firstName} is interested in ${toUserData.firstName}`
@@ -39,7 +40,7 @@ requestRouter.post('/send/:status/:toUserId', userAuth, async (req, res) => {
           data: connectionRequestData,
         });
     } catch (err) {
-        res.status(400).send(err)
+        res.status(400).send(err.message)
     }
 })
 
