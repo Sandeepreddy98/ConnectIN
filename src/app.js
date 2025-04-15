2d47d54f76057c89a26ea366bcb8c64b546d45e9
Sandeep <g.sandeepreddy98@gmail.com>
2025-04-15 23:49:05 +0530

    [NEW] Added CORS Description : Added CORS for accepting cross origin requests and added origin & credentials.

--- src/app.js
+++ src/app.js
@@ -3,12 +3,18 @@ const mongodb = require('../config/database')
 const app = express();
 const cookieParser = require('cookie-parser');
 require('dotenv').config()
+const cors = require('cors');
 const authRouter = require('../routes/auth');
 const profileRouter = require('../routes/profile');
 const requestRouter = require('../routes/request');
 const userRouter = require('../routes/user');
 const postRouter = require('../routes/post');
 
+//CORS for handle cross origin requests
+app.use(cors({
+    origin : process.env.CLIENT_URI,
+    credentials : true
+}))
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cookieParser())
